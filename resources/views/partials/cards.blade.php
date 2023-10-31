9d301d4c23e3ab6d3960991eabc7e55b5c0e79de
francescomascellino <franesco.mascellino@gmail.com>
2023-10-31 19:22:57 +0100

    link single comic details by clicking on card

--- resources/views/partials/cards.blade.php
+++ resources/views/partials/cards.blade.php
@@ -3,20 +3,19 @@
 
         <div class="card bg-transparent">
 
-            {{-- <a href="{{ route('comic_details', $comic->id) }}"> --}}
-            {{-- SI PUO' PASSARE UN VALORE NELLA ROUTE --}}
-            <div class="p-1 dc-coverContainer" style="background-image: url({{ $comic->thumb }})">
+            <a href="{{ route('comics.show', $comic->id) }}">
+                <div class="p-1 dc-coverContainer" style="background-image: url({{ $comic->thumb }})">
 
-            </div>
+                </div>
 
-            <div class="card-body p-0 pt-3">
+                <div class="card-body p-0 pt-3">
 
-                {{-- <a href="{{ route('comic_details', $id) }}">LINK FUMETTO ID {{$id}}</a> --}}
+                    <a href="{{ route('comics.show', $comic->id) }}">LINK FUMETTO ID {{ $comic->id }}</a>
 
-                <p class="card-title text-uppercase">{{ $comic->series }}</p>
-                <p class="card-text text-capitalize">{{ $comic->type }}, price: {{ $comic->price }}</p>}
-            </div>
-            {{-- </a> --}}
+                    <p class="card-title text-uppercase">{{ $comic->series }}</p>
+                    <p class="card-text text-capitalize">{{ $comic->type }}, price: {{ $comic->price }}</p>}
+                </div>
+            </a>
 
         </div>
 
